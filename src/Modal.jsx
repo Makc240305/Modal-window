7ce38e657bb01e722a18b718f8601c1106d8ff8e
Makc240305 <maxim240305@gmail.com>
2025-01-28 22:37:01 +0100

    Added a new function to close the window when you click behind the .modal window, and fixed the image size on mobile devices

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -16,6 +16,7 @@ export default function Modal({open, setOpen}) {
         </svg>
         <img src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExamUxZmR0ZXFwaHQzNnlncm04a2dyN255Mm9yN3g3ZWpjYTJtdTFmbyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3oKIPnAiaMCws8nOsE/giphy.gif" />
       </div>
+      <div className="closure" onClick={() => {setOpen(false)}} />
     </div>
   );
 }
